ab7c19f04b8fe390a53087efc710941ac02d191f
Troy Boettger <troy.boettger@gmail.com>
2020-09-08 09:52:52 -0400

    add more unit tests

--- src/components/items/ItemsList.component.vue
+++ src/components/items/ItemsList.component.vue
@@ -2,30 +2,44 @@
 	<div>
 		<h3>My Items:</h3>
 		<ul>
-			<li v-for="item in items"
-			:key="item.id"
-			@click="onItemClick(item)"
-			>
-			{{ item.name }}</li>
+			<ItemComponent v-for="item in items" :key="item.id" :model="item" @click="onItemClick" />
 		</ul>
 	</div>
 </template>
 
 <script lang="ts">
-	import { Component, Prop, Vue } from 'vue-property-decorator';
-	import { IItem } from '@/models/items/IItem'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { IItem } from "@/models/items/IItem";
+import ItemComponent from "@/components/items/children/Item.component.vue";
+// import { Loader } from "@/components/shared";
 
-	@Component
-	export default class  ItemsListComponent extends Vue {
-		@Prop() items!: IItem[] 
+@Component({
+	components: {
+		//	Loader,
+		ItemComponent,
+	},
+})
+export default class ItemsListComponent extends Vue {
+	@Prop() items!: IItem[];
 
-		onItemClick(item: IItem) {
-			item.selected = !item.selected
-			console.log('onClickItem', item.id, item.selected)
-		}
+	private get loading(): boolean {
+		return this.$store.state.loading;
 	}
+
+	onItemClick(item: IItem) {
+		item.selected = !item.selected;
+		console.log("onClickItem", item.id, item.selected);
+	}
+}
 </script>
 
-<style scoped>
-
+<style lang="scss" >
+ul {
+	list-style-type: none;
+	margin-block-start: 0;
+	margin-block-end: 0;
+	margin-inline-start: 0px;
+	margin-inline-end: 0;
+	padding-inline-start: 0px;
+}
 </style>
--- tests/unit/components/items/Item.component.spec.ts
+++ tests/unit/components/items/Item.component.spec.ts
@@ -23,6 +23,47 @@ describe('Item.component.vue', () => {
 		expect(domEl.text()).to.equal('Unit test item 1')
 		expect(wrapper.text()).to.include('Unit test item 1')
 	})
+
+	it('has expected css class when selected is false', () => {
+		const model: IItem = {
+			id: 1,
+			name: 'Unit test item 1',
+			selected: false,
+		}
+
+		const wrapper = shallowMount(ItemComponent, {
+			propsData: {
+				model: model,
+			},
+		})
+		const classes = wrapper.classes()
+		expect(classes)
+			.to.be.an('array')
+			.that.includes('item')
+		expect(classes)
+			.to.be.an('array')
+			.that.does.not.include('selected')
+	})
+
+	it('has selected class when selected is true', () => {
+		const model: IItem = {
+			id: 1,
+			name: 'Unit test item 1',
+			selected: true /*setting selected = true here */,
+		}
+		const wrapper = shallowMount(ItemComponent, {
+			propsData: {
+				model: model,
+			},
+		})
+		const classes = wrapper.classes()
+		expect(classes)
+			.to.be.an('array')
+			.that.includes('item')
+		expect(classes)
+			.to.be.an('array')
+			.that.includes('selected')
+	})
 })
 
 //STOPPED AT PG. 58
